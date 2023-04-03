7d12a1feb722dcab63c620579141128a76a27a49
Julia Outumuro <juliaoutumuro@MacBook-Air.local>
2023-04-03 22:12:39 +0200

    init project struct

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,6 @@
+import "./services/web/registerServiceWorker";
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./routes";
 
 createApp(App).use(router).mount("#app");
