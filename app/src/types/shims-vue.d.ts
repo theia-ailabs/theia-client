31b06e1ff4903ab2ad99ee28d018631279dcfbba
Julia Outumuro <juliaoutumuro@MacBook-Air.local>
2023-04-03 21:43:16 +0200

    add interfaces and types

--- app/src/types/shims-vue.d.ts
+++ app/src/types/shims-vue.d.ts
@@ -1,6 +1,31 @@
 /* eslint-disable */
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.wav" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.mp3" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.mp4" {
+  const value: any;
+  export default value;
 }
