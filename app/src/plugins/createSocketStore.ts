9ec33dfbe759067c01d72f893f5c882e015a83d2
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-05 00:55:49 +0200

    general refactor

--- app/src/plugins/createSocketStore.ts
+++
@@ -1,8 +0,0 @@
-import { Socket } from 'socket.io-client';
-
-export default function createSocketStore (socket: Socket) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return (store: any) => {
-    store.state.$socket = socket;
-  };
-}
