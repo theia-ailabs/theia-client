69af81760b983e74912fc2eafd944e2a7f161a02
JRosSx91 <hazee.1491@gmail.com>
2023-04-04 11:32:35 +0200

    adding assets

---
+++ app/src/components/CanvasComp.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+function foo() {
+  return;
+}
+</script>
+<template>
+  <div class="flex">
+    <button :on-click="foo">Click me</button>
+  </div>
+</template>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
