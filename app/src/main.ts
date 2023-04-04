a1f1c1fcabce41b43f100ea3af08d3a84843bff8
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-05 00:53:42 +0200

    general refactor

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,7 +1,12 @@
 import "./services/web/registerServiceWorker";
 import "./assets/css/tailwind.css";
+import "./assets/css/wallets.css";
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./routes";
 
-createApp(App).use(router).mount("#app");
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia).use(router).mount("#app");
