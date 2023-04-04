d5f5c52cbcf2cd1c9d39e585680bb99c460f309e
JRosSx91 <hazee.1491@gmail.com>
2023-04-04 17:51:21 +0200

    updating navbar comp

--- app/src/components/NavbarComp.vue
+++ app/src/components/NavbarComp.vue
@@ -11,8 +11,11 @@ export default defineComponent({
 });
 </script>
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/chat"></router-link>
+  <nav class="fixed pt-4 flex bg-transparent justify-center w-full top-0">
+    <button (click)="scrollTop()" class="relative z-50 flex bg-transparent
+          justify-center
+          items-center py-4">
+      <img class="h-16" src="./../assets/img/png/logo3.png" alt="logo">
+    </button>
   </nav>
 </template>
