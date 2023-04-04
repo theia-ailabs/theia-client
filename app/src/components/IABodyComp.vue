174fe18faa6fa7bf4315e1cc3ff1135f4361e663
JRosSx91 <hazee.1491@gmail.com>
2023-04-04 18:03:24 +0200

    creating both svg ia files

---
+++ app/src/components/IABodyComp.vue
@@ -0,0 +1,15 @@
+<script></script>
+<template>
+    <div class="bg-transparent z-0 relative h-screen">
+        <div v-if="listening()" class="">
+            <button (click)="toTalk()">
+                <app-svg-ia />
+            </button>
+        </div>
+        <div v-if="talking()">
+            <button (click)="toListen()">
+                <app-svg-talking />
+            </button>
+        </div>
+    </div>
+</template>
