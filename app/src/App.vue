9a418289b960d1dc3bc42db7e54e953446842afc
JRosSx91 <hazee.1491@gmail.com>
2023-04-04 17:45:33 +0200

    updating mainview

--- app/src/App.vue
+++ app/src/App.vue
@@ -9,16 +9,19 @@
   text-align: center;
   color: #2c3e50;
 }
+
 html body {
   margin: 0;
   padding: 0;
   overflow-x: hidden;
 }
+
 ::-webkit-scrollbar {
   width: 3px;
   height: 3px;
   background-color: transparent !important;
 }
+
 ::-webkit-scrollbar-track {
   background-color: rgba(179, 179, 179, 0.692) !important;
   border-radius: 1rem;
@@ -26,13 +29,12 @@ html body {
   margin-top: 0.4rem;
   border-left: 1px solid rgba(211, 211, 211, 0) !important;
 }
+
 ::-webkit-scrollbar-thumb {
   border-radius: 20px;
-  background: linear-gradient(
-    30deg,
-    rgba(140, 208, 11, 0.83),
-    #cf0ccfbe
-  ) !important;
+  background: linear-gradient(30deg,
+      rgba(140, 208, 11, 0.83),
+      #cf0ccfbe) !important;
   box-shadow: inset 0 0 6px rgb(206, 9, 212) !important;
 }
 </style>
--- app/src/views/MainView.vue
+++ app/src/views/MainView.vue
@@ -1,9 +1,18 @@
 <script lang="ts" setup>
 import NavbarComp from "../components/NavbarComp.vue";
+import IABodyComp from "@/components/IABodyComp.vue";
+import ChatComp from "@/components/ChatComp.vue";
 </script>
 <template>
-  <div class="home">
-    <NavbarComp />
-    <p>This is the main page</p>
+  <div class="home w-full h-screen">
+    <NavbarComp class="z-50 fixed w-full bg-black" />
+    <div id="main-display" class="bg-black">
+      <div class="z-0 w-full h-screen mt-24">
+        <IABodyComp class="h-screen" />
+      </div>
+      <div class="w-full h-screen">
+        <ChatComp />
+      </div>
+    </div>
   </div>
 </template>
