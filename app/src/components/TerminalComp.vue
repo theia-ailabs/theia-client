4d7065dfc44d07cab9036c3ea29b60733cf90554
JRosSx91 <hazee.1491@gmail.com>
2023-04-05 15:53:16 +0200

    adding template terminal

--- app/src/components/TerminalComp.vue
+++ app/src/components/TerminalComp.vue
@@ -1,5 +1,18 @@
 <script></script>
 <template>
-    
+    <div class="workspace w-full">
+    <!-- <app-svg-spacetravel /> -->
+    <div class="container-text w-full">
+        <div id="appear1" style="display:none;">
+            <a href="/">Connecting to Theia Systems...&nbsp;&nbsp;<span>&nbsp;</span></a>
+        </div>
+        <div id="appear2" style="display:none;">
+            <a href="/">Confirming human identity...&nbsp;&nbsp;<span>&nbsp;</span></a>
+        </div>
+        <div id="appear3" style="display:none;">
+            <a href="/">Connected!&nbsp;&nbsp;<span>&nbsp;</span></a>
+        </div>
+    </div>
+</div>
 </template>
 <style scoped></style>
