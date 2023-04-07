0ca97504ed963dee2208f4b59ff7c5af460b5a5c
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-07 19:17:22 +0200

    add webpack config + crypto polyfills

---
+++ app/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  resolve: {
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      assert: require.resolve("assert"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      os: require.resolve("os-browserify"),
+      url: require.resolve("url"),
+    },
+  },
+};
