952800328794c789582afbc791f1c32390a829f5
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-07 22:29:58 +0200

    add SignupModal.vue

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   resolve: {
     fallback: {
-      crypto: false,
-      stream: false,
+      fs: false, // require.resolve("fs"),
+      crypto: false, // require.resolve("crypto-browserify"),
+      stream: false, // require.resolve("stream-browserify"),
+      os: false, // require.resolve("os-browserify/browser"),
+      path: false, // require.resolve("path-browserify"),
     },
   },
   babel: { compact: true },
