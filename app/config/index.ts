35250991e8e1059fc3540c470b57d666b0388e85
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-08 14:24:28 +0200

    init polyfills

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,5 +1,1 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const SERVER_URI =
-  String(process.env.SERVER_URI) || "http://localhost:3000";
+export const SERVER_URI = "http://localhost:3000";
--- app/vue.config.js
+++ app/vue.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 module.exports = defineConfig({
   publicPath: "",
   transpileDependencies: true,
   configureWebpack: {
-    plugins: [new NodePolyfillPlugin()],
+    plugins: [new NodePolyfillPlugin(), new Dotenv()],
     stats: { warnings: false },
   },
   chainWebpack: (config) => {
