9c1950fbaf08ecf850270ba21e41dd00b013c477
JRosSx91 <hazee.1491@gmail.com>
2023-04-09 12:11:22 +0200

    provide store in main.ts

--- app/src/main.ts
+++ app/src/main.ts
@@ -8,8 +8,10 @@ import Notifications from "@kyvg/vue3-notification";
 import velocity from "velocity-animate";
 import router from "./routes";
 import App from "./App.vue";
+import store from "./services/store";
 
 useWorkspace(); // Init Solana Wallets
 const app = createApp(App);
 const pinia = createPinia();
+app.provide('store', store);
 app.use(pinia).use(router).use(Notifications, { velocity }).mount("#app");
