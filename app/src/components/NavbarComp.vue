8c72246863bdd55c35475d0b8e59c5db69f299f5
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-11 00:32:25 +0200

    add fixed WavesComp

--- app/src/components/NavbarComp.vue
+++ app/src/components/NavbarComp.vue
@@ -1,10 +1,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import useStore from "../services/store";
+// import WavesComp from "./WavesComp.vue";
+import InputComp from "./InputComp.vue";
 import logo from "../assets/img/png/logo.png";
 import profile from "../assets/img/png/profile.png";
 
 export default defineComponent({
+  components: { InputComp },
   setup() {
     const store = useStore();
     return {
@@ -25,22 +28,11 @@ export default defineComponent({
         <img class="h-14" :src="logo" alt="Theia logo" />
       </button>
     </div>
+    <div>
+      <!-- <WavesComp /> -->
+    </div>
     <div class="flex -mt-4">
-      <form id="input-chat" class="flex relative justify-center w-full">
-        <textarea
-          v-model="store.input"
-          class="p-1 my-2 py-2 text-center border border-purple-300/50 bg-black/50 text-white text-xs shadow-inner shadow-yellow-400/50 rounded-xl z-50 hover:border-purple-400 hover:shadow-yellow-400/80"
-        >
-        </textarea>
-        <div class="px-2 mt-4">
-          <button
-            class="ml-2 p-2 text-xs rounded-full border text-white/50 border-purple-400/30 shadow-sm shadow-yellow-400/50 hover:text-white hover:border-purple-400 hover:shadow-yellow-400"
-            @click="store.inputMessage()"
-          >
-            Send
-          </button>
-        </div>
-      </form>
+      <InputComp />
     </div>
     <div class="mr-12 -mt-1">
       <button :click="store.scrollTop()">
