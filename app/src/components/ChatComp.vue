04c47c1a0d01ea6a4eb0b78eff4bb903bf058d13
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-11 18:50:01 +0200

    added loader in chat msg

--- app/src/components/ChatComp.vue
+++ app/src/components/ChatComp.vue
@@ -39,8 +39,11 @@ export default defineComponent({
                 <div
                   class="text-xs text-white p-2 px-8 font-semibold rounded-xl bg-black/30 rounded-bl-none w-[250px] sm:w-[320px] lg:w-[540px] shadow-sm shadow-purple-400 border border-yellow-400/30"
                 >
-                  <div v-if="x.theia.text.includes('Thinking')">
-                    <img :src="loader" alt="Theia is thinking" />
+                  <div
+                    v-if="x.theia.text.includes('Thinking')"
+                    class="flex w-full justify-center"
+                  >
+                    <img :src="loader" alt="Theia is thinking" class="w-56" />
                   </div>
                   <div v-else>
                     <AudioWaveComp
