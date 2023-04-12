6ce39e62660f9b45be8b03d0372cd0b3085be71c
JRosSx91 <hazee.1491@gmail.com>
2023-04-12 15:48:39 +0200

    create btn nav

--- app/src/components/NavbarComp.vue
+++ app/src/components/NavbarComp.vue
@@ -28,23 +28,23 @@ export default defineComponent({
     class="z-50 fixed pt-4 flex justify-between w-full top-0"
     :class="store.dark ? 'bg-black/90' : 'bg-white/90'"
   >
-    <div class="flex">
-      <button>
+    <div class="flex ml-12">
+      <button class="mx-3">
         <img :src="chat" alt="chat" />
       </button>
-      <button>
+      <button class="mx-3">
         <img :src="calendar" alt="calendar" />
       </button>
-      <button>
+      <button class="mx-3">
         <img :src="email" alt="email" />
       </button>
     </div>
-    <div class="ml-32"></div>
-    <div class="m-1">
+    <div class="m-1 mx-auto">
       <button :click="store.scrollTop()" class="flex justify-center">
         <img class="h-12 lg:h-16" :src="logo" alt="Theia logo" />
       </button>
     </div>
+    <div class="mr-16"></div>
     <div class="flex flex-wrap mx-4 sm:mr-12 -mt-2">
       <!-- <WalletsBtn /> -->
       <button :click="store.scrollTop()">
