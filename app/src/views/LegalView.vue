dc83cbbca8d1eb4b1ade7f83b6febda6213d6860
JRosSx91 <hazee.1491@gmail.com>
2023-04-14 14:42:40 +0200

    styles legal

--- app/src/views/LegalView.vue
+++ app/src/views/LegalView.vue
@@ -13,38 +13,38 @@ export default defineComponent({
 </script>
 <template>
   <div class="container mx-auto px-4 py-8">
-    <h1 class="text-3xl font-bold mb-6">Términos de servicio</h1>
-    <p>Última actualización: 14/04/2023</p>
+    <h1 class="text-5xl font-bold mb-3">Términos de servicio</h1>
+    <p class="mb-4">Última actualización: 14/04/2023</p>
 
-    <ol class="list-decimal list-outside pl-6 space-y-2">
-      <li class="mb-2">
-        <strong class="font-semibold">Introducción</strong>
-        <p>
+    <ol class="list-outside pl-6 text-left w-[90%] mx-auto">
+      <li class="mb-6">
+        <strong class="font-semibold text-3xl">Introducción</strong>
+        <p class="mt-2">
           Bienvenido a Theia. Estos Términos de servicio (en adelante,
           "Términos") rigen tu acceso y uso de nuestro sitio web, aplicaciones y
           otros productos y servicios (colectivamente, los "Servicios").
         </p>
       </li>
-      <li class="mb-2">
-        <strong class="font-semibold">Registro de cuentas</strong>
-        <p>
+      <li class="mb-6">
+        <strong class="font-semibold text-3xl">Registro de cuentas</strong>
+        <p class="mt-2">
           Para utilizar algunos de nuestros Servicios, es posible que debas
           registrarte y crear una cuenta. Al crear una cuenta, aceptas
           proporcionar información precisa, actual y completa sobre ti.
         </p>
       </li>
-      <li class="mb-2">
-        <strong class="font-semibold">Uso aceptable</strong>
-        <p>
+      <li class="mb-6">
+        <strong class="font-semibold text-3xl">Uso aceptable</strong>
+        <p class="mt-2">
           Te comprometes a no utilizar los Servicios de manera que viole
           cualquier ley aplicable, infrinja los derechos de propiedad
           intelectual, o de cualquier otra manera que cause daño a terceros o a
           los Servicios.
         </p>
       </li>
-      <li class="mb-2">
-        <strong class="font-semibold">Propiedad intelectual</strong>
-        <p>
+      <li class="mb-6">
+        <strong class="font-semibold text-3xl">Propiedad intelectual</strong>
+        <p class="mt-2">
           Los Servicios y su contenido original, características y
           funcionalidades son y seguirán siendo propiedad exclusiva de Theia y
           sus licenciantes. Los Servicios están protegidos por derechos de
@@ -52,9 +52,11 @@ export default defineComponent({
           países.
         </p>
       </li>
-      <li class="mb-2">
-        <strong class="font-semibold">Limitación de responsabilidad</strong>
-        <p>
+      <li class="mb-6">
+        <strong class="font-semibold text-3xl"
+          >Limitación de responsabilidad</strong
+        >
+        <p class="mt-2">
           En ningún caso Theia ni sus directores, empleados o afiliados serán
           responsables de daños indirectos, incidentales, especiales,
           consecuentes o punitivos, incluidos, entre otros, la pérdida de
@@ -62,18 +64,18 @@ export default defineComponent({
           como resultado del acceso o uso de los Servicios.
         </p>
       </li>
-      <li class="mb-2">
-        <strong class="font-semibold">Cambios en los términos</strong>
-        <p>
+      <li class="mb-6">
+        <strong class="font-semibold text-3xl">Cambios en los términos</strong>
+        <p class="mt-2">
           Nos reservamos el derecho de modificar o reemplazar estos Términos en
           cualquier momento a nuestra discreción. Si hacemos cambios en los
           Términos, te lo notificaremos actualizando la fecha en la parte
           superior de esta página.
         </p>
       </li>
-      <li class="mb-2">
-        <strong class="font-semibold">Contacto</strong>
-        <p>
+      <li class="mb-6">
+        <strong class="font-bold text-3xl">Contacto</strong>
+        <p class="mt-2">
           Si tienes alguna pregunta sobre estos Términos, por favor, no dudes en
           contactarnos a través de theia.ailabs@gmail.com.
         </p>
