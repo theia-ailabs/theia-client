54304268d7b5fd4b854691311c8d418c9267365f
JRosSx91 <hazee.1491@gmail.com>
2023-04-14 15:24:54 +0200

    creating modals legal and privacy

---
+++ app/src/components/modules/modals/FirstTimePrivacyModal.vue
@@ -0,0 +1,28 @@
+<template>
+    <teleport to="body">
+        <notifications position="top left" class="mt-2" animation-type="velocity" />
+        <div>
+        <LegalView></LegalView>
+        <form>
+            <input type="checkbox" required>
+            <p> Acepto la Política de privacidad.</p>
+            <button type="button">Enviar</button>
+        </form>
+    </div>
+    </teleport>
+</template>
+<script lang="ts">
+import {defineComponent} from "vue";
+import PrivacyView from "@/views/PrivacyView.vue";
+import useStore from "@/services/store";
+
+export default defineComponent({
+    setup() {
+        const store = useStore();
+        return {
+            store,
+        };
+    },
+    components: { PrivacyView }
+});
+</script>
