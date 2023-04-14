b35b287b6056d212afaa82af5cdd14f4a05f0acc
JRosSx91 <hazee.1491@gmail.com>
2023-04-14 12:51:59 +0200

    template legal page

--- app/src/views/LegalView.vue
+++ app/src/views/LegalView.vue
@@ -14,8 +14,27 @@ export default defineComponent({
 });
 </script>
 <template>
-  <div class="m-0">
-    <NavbarComp />
-    <p>This is the legal page</p>
+  <div>
+    <h1>Términos de servicio</h1>
+    <p>Última actualización: 14/04/2023</p>
+
+    <h2>1. Introducción</h2>
+    <p>
+      Bienvenido a Theia. Estos Términos de servicio
+      (en adelante, "Términos") rigen tu acceso y uso de nuestro sitio web, aplicaciones
+      y otros productos y servicios (colectivamente, los "Servicios").
+    </p>
+
+    <!-- Añade más secciones para cubrir temas como el registro de cuentas, uso aceptable, propiedad intelectual, limitación de responsabilidad, etc. -->
+
+    <h2>X. Cambios en los Términos</h2>
+    <p>
+      Nos reservamos el derecho de modificar o reemplazar estos Términos en cualquier momento a nuestra discreción. Si hacemos cambios en los Términos, te lo notificaremos actualizando la fecha en la parte superior de esta página.
+    </p>
+
+    <h2>X+1. Contacto</h2>
+    <p>
+      Si tienes alguna pregunta sobre estos Términos, por favor, no dudes en contactarnos a través de (proporcionar información de contacto, como dirección de correo electrónico).
+    </p>
   </div>
 </template>
