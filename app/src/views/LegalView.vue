4eab61b0f2f13503179f640173d996069551a677
JRosSx91 <hazee.1491@gmail.com>
2023-04-14 13:32:19 +0200

    upgrading legal and privacy views

--- app/src/views/LegalView.vue
+++ app/src/views/LegalView.vue
@@ -25,16 +25,34 @@ export default defineComponent({
       y otros productos y servicios (colectivamente, los "Servicios").
     </p>
 
-    <!-- Añade más secciones para cubrir temas como el registro de cuentas, uso aceptable, propiedad intelectual, limitación de responsabilidad, etc. -->
+    <h2>2. Registro de cuentas</h2>
+    <p>
+      Para utilizar algunos de nuestros Servicios, es posible que debas registrarte y crear una cuenta. Al crear una cuenta, aceptas proporcionar información precisa, actual y completa sobre ti.
+    </p>
 
-    <h2>X. Cambios en los Términos</h2>
+    <h2>3. Uso aceptable</h2>
+    <p>
+      Te comprometes a no utilizar los Servicios de manera que viole cualquier ley aplicable, infrinja los derechos de propiedad intelectual, o de cualquier otra manera que cause daño a terceros o a los Servicios.
+    </p>
+
+    <h2>4. Propiedad intelectual</h2>
+    <p>
+      Los Servicios y su contenido original, características y funcionalidades son y seguirán siendo propiedad exclusiva de Theia y sus licenciantes. Los Servicios están protegidos por derechos de autor, marcas comerciales y otras leyes tanto de España como de otros países.
+    </p>
+
+    <h2>5. Limitación de responsabilidad</h2>
+    <p>
+      En ningún caso Theia ni sus directores, empleados o afiliados serán responsables de daños indirectos, incidentales, especiales, consecuentes o punitivos, incluidos, entre otros, la pérdida de beneficios, datos, uso, buena voluntad u otras pérdidas intangibles, como resultado del acceso o uso de los Servicios.
+    </p>
+
+    <h2>6. Cambios en los Términos</h2>
     <p>
       Nos reservamos el derecho de modificar o reemplazar estos Términos en cualquier momento a nuestra discreción. Si hacemos cambios en los Términos, te lo notificaremos actualizando la fecha en la parte superior de esta página.
     </p>
 
-    <h2>X+1. Contacto</h2>
+    <h2>7. Contacto</h2>
     <p>
-      Si tienes alguna pregunta sobre estos Términos, por favor, no dudes en contactarnos a través de (proporcionar información de contacto, como dirección de correo electrónico).
+      Si tienes alguna pregunta sobre estos Términos, por favor, no dudes en contactarnos a través de theia.ailabs@gmail.com.
     </p>
   </div>
 </template>
--- app/src/views/PrivacyView.vue
+++ app/src/views/PrivacyView.vue
@@ -20,34 +20,75 @@ export default defineComponent({
       <p>
         En Theia, nos preocupamos por tu privacidad. Esta Política de privacidad describe cómo recopilamos, utilizamos, divulgamos y protegemos tus datos personales cuando utilizas nuestros Servicios.
       </p>
-      <h2>2. Registro de cuentas</h2>
+      <h2>2. Información que recopilamos</h2>
     <p>
-      Para utilizar algunos de nuestros Servicios, es posible que debas registrarte y crear una cuenta. Al crear una cuenta, aceptas proporcionar información precisa, actual y completa sobre ti.
+      Cuando utilizas nuestros Servicios, podemos recopilar información sobre ti, como:
+      <ul>
+        <li>Información de registro: nombre, dirección de correo electrónico, contraseña y otros datos que nos proporciones al registrarte en nuestra plataforma.</li>
+        <li>Información de perfil: información que nos proporciones en tu perfil de usuario, como tu foto de perfil, intereses y preferencias.</li>
+        <li>Información del dispositivo: información sobre el dispositivo que utilizas para acceder a nuestros Servicios, como el tipo de dispositivo, el sistema operativo, la dirección IP y la información de la ubicación.</li>
+        <li>Información de uso: información sobre cómo utilizas nuestros Servicios, como las páginas que visitas, las funciones que utilizas y el tiempo que pasas en nuestra plataforma.</li>
+      </ul>
     </p>
 
-    <h2>3. Uso aceptable</h2>
+    <h2>3. Uso de la información</h2>
     <p>
-      Te comprometes a no utilizar los Servicios de manera que viole cualquier ley aplicable, infrinja los derechos de propiedad intelectual, o de cualquier otra manera que cause daño a terceros o a los Servicios.
+      Utilizamos la información que recopilamos para:
+      <ul>
+        <li>Proporcionar y mejorar nuestros Servicios.</li>
+        <li>Personalizar tu experiencia en nuestra plataforma.</li>
+        <li>Responder a tus solicitudes y brindarte soporte.</li>
+        <li>Comunicarnos contigo sobre actualizaciones, promociones y otras noticias relacionadas con nuestros Servicios.</li>
+        <li>Realizar análisis y estudios para mejorar nuestros Servicios.</li>
+      </ul>
     </p>
 
-    <h2>4. Propiedad intelectual</h2>
+    <h2>4. Compartir información con terceros</h2>
     <p>
-      Los Servicios y su contenido original, características y funcionalidades son y seguirán siendo propiedad exclusiva de (nombre de tu empresa) y sus licenciantes. Los Servicios están protegidos por derechos de autor, marcas comerciales y otras leyes tanto de (tu país) como de otros países.
+      No vendemos ni compartimos tu información personal con terceros para fines de marketing sin tu consentimiento. Podemos compartir tu información con:
+      <ul>
+        <li>Proveedores de servicios que nos ayudan a operar y mantener nuestros Servicios.</li>
+        <li>En caso de venta, fusión o adquisición de nuestra empresa, podemos transferir tu información como parte de dicha transacción.</li>
+        <li>Cuando sea necesario para cumplir con la ley o proteger nuestros derechos, propiedad o seguridad, o los de nuestros usuarios o terceros.</li>
+      </ul>
     </p>
 
-    <h2>5. Limitación de responsabilidad</h2>
+    <h2>5. Protección de la información</h2>
     <p>
-      En ningún caso (nombre de tu empresa) ni sus directores, empleados o afiliados serán responsables de daños indirectos, incidentales, especiales, consecuentes o punitivos, incluidos, entre otros, la pérdida de beneficios, datos, uso, buena voluntad u otras pérdidas intangibles, como resultado del acceso o uso de los Servicios.
+      Nos esforzamos por proteger tu información personal utilizando medidas de seguridad técnicas y organizativas adecuadas. Sin embargo, ningún método de transmisión por Internet o almacenamiento electrónico es completamente seguro, por lo que no podemos garantizar la seguridad absoluta de tu información.
+    </p>
+    <h2>6. Tus derechos y opciones</h2>
+    <p>
+      Según las leyes aplicables en tu jurisdicción, puedes tener derechos y opciones con respecto a tu información personal, como:
+      <ul>
+        <li>Acceso: puedes solicitar acceso a la información personal que tenemos sobre ti.</li>
+        <li>Rectificación: puedes solicitarnos que corrijamos cualquier información personal inexacta o incompleta.</li>
+        <li>Eliminación: puedes solicitarnos que eliminemos tu información personal cuando ya no sea necesaria para los fines para los que fue recopilada.</li>
+        <li>Restricción de procesamiento: puedes solicitarnos que restrinjamos el procesamiento de tu información personal en ciertas circunstancias.</li>
+        <li>Portabilidad de datos: puedes solicitarnos que te proporcionemos tu información personal en un formato estructurado, comúnmente utilizado y legible por máquina, o que la transfiramos a otro controlador de datos.</li>
+        <li>Objeción: puedes objetar el procesamiento de tu información personal en ciertas circunstancias.</li>
+        <li>Retirar el consentimiento: si hemos obtenido tu consentimiento para procesar tu información personal, puedes retirarlo en cualquier momento.</li>
+      </ul>
+      Para ejercer tus derechos, ponte en contacto con nosotros a través de theia.ailabs@gmail.com.
     </p>
 
-    <h2>6. Cambios en los Términos</h2>
+    <h2>7. Retención de datos</h2>
     <p>
-      Nos reservamos el derecho de modificar o reemplazar estos Términos en cualquier momento a nuestra discreción. Si hacemos cambios en los Términos, te lo notificaremos actualizando la fecha en la parte superior de esta página.
+      Retenemos tu información personal durante el tiempo que sea necesario para los fines descritos en esta Política de privacidad, a menos que la ley exija o permita un período de retención más largo.
     </p>
 
-    <h2>7. Contacto</h2>
+    <h2>8. Enlaces a sitios web de terceros</h2>
     <p>
-      Si tienes alguna pregunta sobre estos Términos, por favor, no dudes en contactarnos a través de (proporcionar información de contacto, como dirección de correo electrónico).
+      Nuestros Servicios pueden contener enlaces a sitios web de terceros. Esta Política de privacidad no se aplica a la información recopilada por dichos sitios web. Te recomendamos que revises las políticas de privacidad de los sitios web de terceros antes de proporcionarles tu información personal.
+    </p>
+    <h2>9. Cambios en la Política de privacidad</h2>
+    <p>
+      Nos reservamos el derecho de modificar o actualizar esta Política de privacidad en cualquier momento a nuestra discreción. Si hacemos cambios en la Política de privacidad, te lo notificaremos actualizando la fecha en la parte superior de esta página.
+    </p>
+
+    <h2>10. Contacto</h2>
+    <p>
+      Si tienes alguna pregunta sobre esta Política de privacidad, por favor, no dudes en contactarnos a través de theia.ailabs@gmail.com.
     </p>
     </div>
   </template>
