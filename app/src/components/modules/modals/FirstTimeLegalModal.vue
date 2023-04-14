047c2442ec883b291ea2d6cdb2e7f2575533b8cc
JRosSx91 <hazee.1491@gmail.com>
2023-04-14 17:04:19 +0200

    testing chat

--- app/src/components/modules/modals/FirstTimeLegalModal.vue
+++ app/src/components/modules/modals/FirstTimeLegalModal.vue
@@ -1,28 +1,28 @@
 <template>
-    <teleport to="body">
-        <notifications position="top left" class="mt-2" animation-type="velocity" />
-        <div>
-        <LegalView></LegalView>
-        <form>
-            <input type="checkbox" required>
-            <p> Acepto los Términos de Servicio.</p>
-            <button type="button">Enviar</button>
-        </form>
+  <teleport to="body">
+    <notifications position="top left" class="mt-2" animation-type="velocity" />
+    <div>
+      <LegalView />
+      <form>
+        <input type="checkbox" required />
+        <p>Acepto los Términos de Servicio.</p>
+        <button type="button">Enviar</button>
+      </form>
     </div>
-    </teleport>
+  </teleport>
 </template>
 <script lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 import LegalView from "@/views/LegalView.vue";
 import useStore from "@/services/store";
 
 export default defineComponent({
-    setup() {
-        const store = useStore();
-        return {
-            store,
-        };
-    },
-    components: { LegalView }
+  setup() {
+    const store = useStore();
+    return {
+      store,
+    };
+  },
+  components: { LegalView },
 });
 </script>
--- app/src/components/modules/modals/FirstTimePrivacyModal.vue
+++ app/src/components/modules/modals/FirstTimePrivacyModal.vue
@@ -1,28 +1,28 @@
 <template>
-    <teleport to="body">
-        <notifications position="top left" class="mt-2" animation-type="velocity" />
-        <div>
-        <LegalView></LegalView>
-        <form>
-            <input type="checkbox" required>
-            <p> Acepto la Política de privacidad.</p>
-            <button type="button">Enviar</button>
-        </form>
+  <teleport to="body">
+    <notifications position="top left" class="mt-2" animation-type="velocity" />
+    <div>
+      <PrivacyView />
+      <form>
+        <input type="checkbox" required />
+        <p>Acepto la Política de privacidad.</p>
+        <button type="button">Enviar</button>
+      </form>
     </div>
-    </teleport>
+  </teleport>
 </template>
 <script lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 import PrivacyView from "@/views/PrivacyView.vue";
 import useStore from "@/services/store";
 
 export default defineComponent({
-    setup() {
-        const store = useStore();
-        return {
-            store,
-        };
-    },
-    components: { PrivacyView }
+  setup() {
+    const store = useStore();
+    return {
+      store,
+    };
+  },
+  components: { PrivacyView },
 });
 </script>
