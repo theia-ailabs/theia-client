7617a675472bd3fefa1348c9957de49ca10d125a
JRosSx91 <hazee.1491@gmail.com>
2023-04-20 17:06:20 +0200

    loader view

---
+++ app/src/views/LoaderView.vue
@@ -0,0 +1,14 @@
+<template>
+  <LoaderComp />
+</template>
+<script lang="ts">
+import LoaderComp from "@/components/LoaderComp.vue";
+</script>
+<style scoped>
+body {
+  background-color: black;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+</style>
