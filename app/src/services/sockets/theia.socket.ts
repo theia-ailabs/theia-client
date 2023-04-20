6c753a002d670158bb7c3a6bb0b702e85b931d7f
JRosSx91 <hazee.1491@gmail.com>
2023-04-20 17:00:49 +0200

    progress

--- app/src/services/sockets/theia.socket.ts
+++ app/src/services/sockets/theia.socket.ts
@@ -2,13 +2,13 @@ import socket from ".";
 import { State } from "../../interfaces";
 import { avatarSettings } from "../store/default";
 
-export const askTheia = (question: string, _voice = "denis", _speed = 1) => {
+export const askTheia = (question: string, _voice = "larry", _speed = 1) => {
   socket.volatile.emit("askTheia", question, _voice, _speed);
 };
 export const theiaRes = (store: State) => {
   socket.volatile.on("theiaRes", (res: any) => {
     console.log(res);
-    store.chat[0].theia.text = res.text;
+    let i = 0;
     if (res.audio) {
       console.log("audio received");
       console.log(res.audio);
@@ -17,6 +17,16 @@ export const theiaRes = (store: State) => {
       store.avatarMode = "listening";
       store.avatarConfig = avatarSettings["listening"];
       store.rerenderAvatar++;
+    } else {
+      if (res.text) {
+        store.chat[0].theia.text = "";
+        const intervalID = setInterval(() => {
+          store.chat[0].theia.text += res.text[i] || " ";
+          if (res.text.length <= i) clearInterval(intervalID);
+          i++;
+        }, 80);
+        console.log(res.text);
+      }
     }
   });
 };
