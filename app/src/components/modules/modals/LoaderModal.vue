dfd20c9003235f6035d5936b125b45f98463185d
JRosSx91 <hazee.1491@gmail.com>
2023-04-21 13:52:48 +0200

    changes

--- app/src/components/modules/modals/LoaderModal.vue
+++ app/src/components/modules/modals/LoaderModal.vue
@@ -1,24 +1,24 @@
 <template>
-    <teleport to="body">
-        <notifications position="top left" class="mt-2" animation-type="velocity" />
-        <div id="loader" class="out">
-    <div class="fade-in flex">
-      <div class="container">
-        <div class="one common"></div>
-        <div class="two common"></div>
-        <div class="three common"></div>
-        <div class="four common"></div>
-        <div class="five common"></div>
-        <div class="six common"></div>
-        <div class="seven common"></div>
-        <div class="eight common"></div>
-      </div>
-      <div class="bar">
-        <div class="progress"></div>
+  <teleport to="body">
+    <notifications position="top left" class="mt-2" animation-type="velocity" />
+    <div id="loader" class="out">
+      <div class="fade-in flex">
+        <div class="container">
+          <div class="one common"></div>
+          <div class="two common"></div>
+          <div class="three common"></div>
+          <div class="four common"></div>
+          <div class="five common"></div>
+          <div class="six common"></div>
+          <div class="seven common"></div>
+          <div class="eight common"></div>
+        </div>
+        <div class="bar">
+          <div class="progress"></div>
+        </div>
       </div>
     </div>
-  </div>
-    </teleport>
+  </teleport>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
--- app/src/config/index.ts
+++ app/src/config/index.ts
@@ -4,7 +4,7 @@
 // console.log(SERVER_URL);
 import { Connection } from "@solana/web3.js";
 
-export const SERVER_URL = "//theia-server.onrender.com"; // "localhost:3030";
+export const SERVER_URL = "https://theia-server.onrender.com"; // "localhost:3030";
 export const SOLANA_RPC_URL =
   "https://powerful-red-pond.solana-mainnet.discover.quiknode.pro/fadce6b2a03b0ae9ebf644a74bf1eb4e9c620736/";
 export const BLOCKCHAIN_CONNECTION = new Connection(SOLANA_RPC_URL);
