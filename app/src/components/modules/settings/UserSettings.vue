a484ffa10b9686fccb5ac29777af7142df938053
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-23 14:17:14 +0200

    add settings modules & python services

--- app/src/components/modules/settings/UserSettings.vue
+++ app/src/components/modules/settings/UserSettings.vue
@@ -1,8 +1,14 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import Color1Button from "../buttons/Color1Button.vue";
+import Color2Button from "../buttons/Color2Button.vue";
 import useStore from "../../../services/store";
 
 export default defineComponent({
+  components: {
+    Color1Button,
+    Color2Button,
+  },
   setup() {
     const store = useStore();
     return {
@@ -12,5 +18,115 @@ export default defineComponent({
 });
 </script>
 <template>
-  <div class="flex" :class="store.dark ? 'bg-black' : 'bg-white'">template</div>
+  <teleport to="body">
+    <notifications position="top left" class="mt-2" animation-type="velocity" />
+    <div :class="store.settingsModal ? 'block' : 'hidden'">
+      <div
+        ref="modal-backdrop"
+        id="settings-modal"
+        class="fixed inset-0 overflow-y-auto bg-opacity-50"
+        :class="
+          store.dark ? 'bg-gray-900 text-white/80' : 'bg-black/80 text-gray-700'
+        "
+      >
+        <div class="flex items-center justify-center min-h-screen text-center">
+          <div
+            class="absolute py-24 m-6 sm:w-1/3 rounded-xl text-center overflow-hidden border shadow-md p-8 flex flex-col justify-center"
+            :class="[
+              store.dark
+                ? 'bg-black/70 text-gray-100'
+                : 'bg-white/70 text-gray-900',
+              `border-${store.primaryColor}`,
+              `shadow-${store.secondaryColor}`,
+            ]"
+          >
+            <button
+              @click="store.settingsModal = false"
+              class="absolute text-white/80 top-6 right-6 rounded-full border border-gray-600 px-[10px] pb-[4px] hover:text-white hover:bg-slate-900 hover:border-white/50"
+            >
+              x
+            </button>
+            <h1 class="m-4 text-lg sm:text-base text-gray-400 animate-pulse">
+              CUSTOMIZE THEIA
+            </h1>
+            <p class="mt-14 text-xs sm:text-sm text-gray-400 animate-pulse">
+              AI NAME
+            </p>
+            <div class="flex justify-center w-full px-4">
+              <input
+                trype="text"
+                id="ainame"
+                key="ainame"
+                placeholder="Theia"
+                v-model="store.username"
+                class="mt-4 w-96 py-1 px-4 text-lg text-center tracking-widest rounded-3xl mx-22 border inner shadow-inner bg-transparent text-transparent bg-clip-text bg-gradient-to-r"
+                :class="[
+                  store.dark
+                    ? 'bg-black/70 text-gray-100'
+                    : 'bg-white/70 text-gray-900',
+                  `border-${store.primaryColor}`,
+                  `shadow-${store.secondaryColor}`,
+                  `from-${store.primaryColor} to-${store.secondaryColor}`,
+                ]"
+              />
+            </div>
+            <div
+              class="grid grid-cols-2 py-2 mx-2 sm:mx-4 md:mx-8 ml-2 sm:ml-4 md:ml-8 font-semibold text-gray-400 justify-center align-center align-middle"
+            >
+              <div class="flex mt-3 justify-center">Primary Color</div>
+              <div class="flex justify-center">
+                <Color1Button />
+              </div>
+              <div class="flex justify-center mt-3">Secondary Color</div>
+              <div class="flex justify-center">
+                <Color2Button />
+              </div>
+            </div>
+            <div class="flex w-full justify-center">
+              <button
+                class="m-4 mt-8 w-24 max-w-24 p-3 rounded-3xl uppercase text-sm font-bold border border-gray-500 shadow-sm text-transparent bg-clip-text bg-gradient-to-r"
+                :class="[
+                  store.dark
+                    ? 'bg-black/70 text-gray-100'
+                    : 'bg-white/70 text-gray-900',
+                  `hover:border-${store.primaryColor}`,
+                  `hover:shadow-${store.secondaryColor}`,
+                  `from-${store.primaryColor} to-${store.secondaryColor}`,
+                ]"
+                @click="store.settingsModal = false"
+              >
+                CANCEL
+              </button>
+              <router-link
+                to="/theia"
+                class="m-4 mt-8 w-24 max-w-24 p-3 rounded-3xl uppercase text-sm font-bold border border-gray-500 shadow-sm text-transparent bg-clip-text bg-gradient-to-r"
+                :class="[
+                  store.dark
+                    ? 'bg-black/70 text-gray-100'
+                    : 'bg-white/70 text-gray-900',
+                  `hover:border-${store.primaryColor}`,
+                  `hover:shadow-${store.secondaryColor}`,
+                  `from-${store.primaryColor} to-${store.secondaryColor}`,
+                ]"
+                @click="[store.reRender(), (store.settingsModal = false)]"
+              >
+                SAVE
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </teleport>
 </template>
+<style scoped>
+input:focus {
+  border: none !important;
+}
+input:active {
+  border: none !important;
+}
+#settings-modal {
+  z-index: 9999 !important;
+}
+</style>
