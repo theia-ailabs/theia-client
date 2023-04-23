14a997cc3d2687a6084bc34480a2b719d4a291fd
quantium-rock <alexcollsoutumuro@gmail.com>
2023-04-23 14:15:24 +0200

    add settings modules & python services

---
+++ app/src/components/modules/settings/AvatarSettings.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import useStore from "../../../services/store";
+
+export default defineComponent({
+  setup() {
+    const store = useStore();
+    return {
+      store,
+    };
+  },
+});
+</script>
+<template>
+  <div class="flex" :class="store.dark ? 'bg-black' : 'bg-white'">template</div>
+</template>
