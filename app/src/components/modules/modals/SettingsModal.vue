43820f83cff4195b2d40df1a90d69fae0e2b93b7
JRosSx91 <hazee.1491@gmail.com>
2023-04-30 19:27:11 +0200

    adding modals to setting modals

--- app/src/components/modules/modals/SettingsModal.vue
+++ app/src/components/modules/modals/SettingsModal.vue
@@ -36,10 +36,37 @@ export default defineComponent({
     <notifications position="top left" class="mt-2" animation-type="velocity" />
     <div :class="store.settingsModal ? 'block' : 'hidden'">
       <h1 class="text-white">USER SETTINGS</h1>
+      <button
+      class="rounded-full h-8 w-8 m-2 flex justify-center items-center shadow-xl"
+      @click="switchUserModal"
+      :class="
+        store.dark
+          ? 'bg-white/10 shadow-gray-700 border-white/20 hover:bg-gray-600 text-white'
+          : 'bg-white hover:bg-gray-200 border-black/20 text-gray-600'
+      "
+    ></button>
       <UserSettings v-if="store.userModal ? 'block' : 'hidden'" />
       <h1>THEIA SETTINGS</h1>
+      <button
+      class="rounded-full h-8 w-8 m-2 flex justify-center items-center shadow-xl"
+      @click="switchAvatarModal"
+      :class="
+        store.dark
+          ? 'bg-white/10 shadow-gray-700 border-white/20 hover:bg-gray-600 text-white'
+          : 'bg-white hover:bg-gray-200 border-black/20 text-gray-600'
+      "
+    ></button>
       <AvatarSettings v-if="store.avatarModal ? 'block' : 'hidden'" />
       <h1>CONNECTIONS SETTINGS</h1>
+      <button
+      class="rounded-full h-8 w-8 m-2 flex justify-center items-center shadow-xl"
+      @click="switchConnectionModal"
+      :class="
+        store.dark
+          ? 'bg-white/10 shadow-gray-700 border-white/20 hover:bg-gray-600 text-white'
+          : 'bg-white hover:bg-gray-200 border-black/20 text-gray-600'
+      "
+    ></button>
       <ConnectionsSettings v-if="store.connectionModal ? 'block' : 'hidden'" />
     </div>
   </teleport>
